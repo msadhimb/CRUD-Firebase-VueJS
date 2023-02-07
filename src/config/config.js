3058507e56dd0b852dd3ebc505ad08d9b7120407
msadhimb <adhimbaqy72@gmail.com>
2023-02-07 13:25:59 +0700

    fourth commit

--- src/config/config.js
+++ src/config/config.js
@@ -9,12 +9,12 @@ import { getFirestore } from "@firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_API_KEY,
-  authDomain: "crud-firebase-e30f6.firebaseapp.com",
-  projectId: "crud-firebase-e30f6",
-  storageBucket: "crud-firebase-e30f6.appspot.com",
-  messagingSenderId: "51331116803",
-  appId: "1:51331116803:web:7ce7ba49b2f2d503dac336",
-  measurementId: "G-CDB7T0KVKN",
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
